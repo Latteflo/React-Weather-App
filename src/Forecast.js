09843c2466ca74ccaaf0d4dc9b7693811f542309
Latteflo <sf.simion.f@gmail.com>
2022-04-11 22:26:11 +0200

    added 6th day

--- src/Forecast.js
+++ src/Forecast.js
@@ -22,7 +22,7 @@ export default function Forecast(props) {
       <div className="WeatherForecast">
         <div className="row dailyForecast">
           {forecast.map(function(dailyForecast, index) {
-            if (index < 5) {
+            if (index < 6) {
               return (
                 <div className="col" key={index}>
                   <ForecastDay data={dailyForecast} />
